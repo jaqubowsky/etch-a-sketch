2fa8d145f9ecee9d9b279292cfc1345185a1deea
Jakub Nalewajk <38465906+jaqubowsky@users.noreply.github.com>
2022-12-28 22:31:22 +0100

    Add functionality to changing pen and bg color, and clearing board

--- script.js
+++ script.js
@@ -1,6 +1,51 @@
-// SETTING UP DRAWING AREA
+/* CONSTANTS */
 
 const drawingArea = document.querySelector(".draw-area");
+const penColor = document.querySelector(".color-selector");
+const bgColor = document.querySelector(".background-color-selector");
+const eraserBtn = document.querySelector(".eraser-btn");
+const clearBtn = document.querySelector(".clear-btn");
+const gridSizeSlider = document.querySelector(`input[type="range"]`);
+const sliderText = document.querySelector(".grid-text-el");
+const toggleGridLinesBtn = document.querySelector(".grid-line-el");
+
+// SETTING UP DRAWING AREA
+
+createGrid(16);
+createDrawEl(16);
+
+const allDrawEl = document.querySelectorAll(".draw-el");
+
+allDrawEl.forEach((item) => {
+  item.addEventListener("mouseover", (e) => {
+    if (e.buttons == 1 || e.buttons == 3) {
+      item.style.backgroundColor = penColor.value;
+    }
+  });
+});
+
+// BUTTONS FUNCTIONALITY
+
+clearBtn.addEventListener("click", () => {
+  eraseAll();
+});
+
+eraserBtn.addEventListener("toggle", () => {
+  allDrawEl.forEach((item) => {
+    item.addEventListener("mouseover", () => {
+      item.style.backgroundColor = "white";
+    });
+  });
+});
+
+// ABLE TO CHANGE DEAFULT BACKGROUND COLOUR
+
+bgColor.addEventListener("change", () => {
+  drawingArea.style.backgroundColor = bgColor.value;
+});
+
+// FUNCTIONS
+
 function createGrid(num) {
   drawingArea.style.gridTemplateColumns = `repeat(${num}, 1fr)`;
 }
@@ -13,13 +58,10 @@ function createDrawEl(num) {
   }
 }
 
-createGrid(16);
-createDrawEl(16);
-
-const allDrawEl = document.querySelectorAll(".draw-el");
-
-allDrawEl.forEach((item, color) => {
-  item.addEventListener("mouseover", () => {
-    item.style.backgroundColor = color;
+function eraseAll() {
+  allDrawEl.forEach((item) => {
+    item.removeAttribute("style");
   });
-});
+  drawingArea.style.backgroundColor = "";
+  bgColor.value = "#ffffff"
+}
