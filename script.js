bf7ffb9cc0a1d0f558b80cc83d2366866531068e
Jakub Nalewajk <38465906+jaqubowsky@users.noreply.github.com>
2022-12-29 02:12:40 +0100

    Code refactor, add functionality to whole content

--- script.js
+++ script.js
@@ -1,28 +1,31 @@
-/* CONSTANTS */
+/* VARIABLES */
 
 const drawingArea = document.querySelector(".draw-area");
-const penColor = document.querySelector(".color-selector");
-const bgColor = document.querySelector(".background-color-selector");
+let penColor = document.querySelector(".color-selector");
+let bgColor = document.querySelector(".background-color-selector");
 const eraserBtn = document.querySelector(".eraser-btn");
 const clearBtn = document.querySelector(".clear-btn");
-const gridSizeSlider = document.querySelector(`input[type="range"]`);
-const sliderText = document.querySelector(".grid-text-el");
+let gridSizeSlider = document.querySelector(`input[type="range"]`);
+let sliderText = document.querySelector(".grid-text-el");
 const toggleGridLinesBtn = document.querySelector(".grid-line-el");
+const nodes = document.querySelector(".draw-area").childNodes;
+
+let colorBefore = "#000000";
 
 // SETTING UP DRAWING AREA
+sliderText.textContent = `${gridSizeSlider.value} x ${gridSizeSlider.value}`;
+createDrawEl(gridSizeSlider.value);
 
-createGrid(16);
-createDrawEl(16);
-
-const allDrawEl = document.querySelectorAll(".draw-el");
-
-allDrawEl.forEach((item) => {
-  item.addEventListener("mouseover", (e) => {
-    if (e.buttons == 1 || e.buttons == 3) {
-      item.style.backgroundColor = penColor.value;
-    }
-  });
+gridSizeSlider.addEventListener("input", function () {
+  sliderText.textContent = `${gridSizeSlider.value} x ${gridSizeSlider.value}`;
+  createDrawEl(gridSizeSlider.value);
 });
+
+// MOUSE DOWN DETECTION
+
+let mouseDown = false;
+document.body.onmousedown = () => (mouseDown = true);
+document.body.onmouseup = () => (mouseDown = false);
 
 // BUTTONS FUNCTIONALITY
 
@@ -30,15 +33,13 @@ clearBtn.addEventListener("click", () => {
   eraseAll();
 });
 
-eraserBtn.addEventListener("toggle", () => {
-  allDrawEl.forEach((item) => {
-    item.addEventListener("mouseover", () => {
-      item.style.backgroundColor = "white";
-    });
-  });
+toggleGridLinesBtn.addEventListener("click", () => {
+  toggleGridLines();
 });
 
-// ABLE TO CHANGE DEAFULT BACKGROUND COLOUR
+eraserBtn.addEventListener("click", () => {
+  enableEraser();
+});
 
 bgColor.addEventListener("change", () => {
   drawingArea.style.backgroundColor = bgColor.value;
@@ -46,22 +47,49 @@ bgColor.addEventListener("change", () => {
 
 // FUNCTIONS
 
-function createGrid(num) {
-  drawingArea.style.gridTemplateColumns = `repeat(${num}, 1fr)`;
-}
+function createDrawEl(size) {
+  drawingArea.innerHTML = "";
+  drawingArea.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+  drawingArea.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 
-function createDrawEl(num) {
-  for (i = 0; i < num ** 2; i++) {
+  for (i = 0; i < size * size; i++) {
     const drawEl = document.createElement("div");
     drawEl.classList.add("draw-el");
+    drawEl.addEventListener("mouseover", changeColor);
+    drawEl.addEventListener("mousedown", changeColor);
     drawingArea.appendChild(drawEl);
   }
 }
 
 function eraseAll() {
-  allDrawEl.forEach((item) => {
-    item.removeAttribute("style");
-  });
+  drawingArea.innerHTML = "";
+  gridSizeSlider.value = 32;
+  sliderText.textContent = `${gridSizeSlider.value} x ${gridSizeSlider.value}`;
+  createDrawEl(gridSizeSlider.value);
   drawingArea.style.backgroundColor = "";
-  bgColor.value = "#ffffff"
+  bgColor.value = "#ffffff";
+}
+
+function changeColor(e) {
+  if (e.type === "mouseover" && !mouseDown) return;
+  e.target.style.backgroundColor = penColor.value;
+}
+
+function toggleGridLines() {
+  for (i = 0; i < nodes.length; i++) {
+    if (nodes[i].style.border) {
+      nodes[i].style.border = "";
+    } else {
+      nodes[i].style.border = "1px solid black";
+    }
+  }
+}
+
+function enableEraser() {
+  if (penColor.value === bgColor.value) {
+    penColor.value = colorBefore;
+  } else {
+    colorBefore = penColor.value;
+    penColor.value = bgColor.value;
+  }
 }
